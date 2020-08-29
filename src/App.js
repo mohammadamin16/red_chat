826aaa21e5d9054f161aeca09661ec07686f5bb3
Amin <toutounchi.ma@gmail.com>
2020-08-29 13:24:24 +0430

    very basic functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component, useRef, useState} from 'react';
 import './App.css';
+import Message from "./Message";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = 'ws://localhost:8000/ws/chat'
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.ws = new WebSocket(url)
+        this.state = {
+            message: '',
+            messages: ['Hi', 'Hello']
+        }
+    }
+
+
+    componentDidMount() {
+        this.ws.onopen = () => {
+            // on connecting, do nothing but log it to the console
+            console.log('connected')
+        }
+
+        this.ws.onmessage = (e) => {
+            const data = JSON.parse(e.data)
+            console.log(data)
+            let new_messages = this.state.messages
+            new_messages.push(data['message'])
+            this.setState(new_messages)
+        }
+
+        this.ws.onclose = () => {
+            console.log('disconnected')
+            // automatically try to reconnect on connection loss
+
+        }
+    }
+
+    send = () => {
+        this.ws.send(JSON.stringify({'message': this.state.message}))
+    }
+
+    render() {
+        return (
+
+            <div className="App">
+                {this.state.messages.map((message) => (
+                    <Message
+                        body={message}
+                    />
+                ))}
+
+                <input
+                    onChange={(e) => {
+                        this.setState({message:e.target.value})
+                    }}
+                />
+                <button onClick={this.send}>Send</button>
+
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Message.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default function Message(props){
+  return (
+    <p
+      className='message'
+    >{props.body}</p>
+  );
+}
+
+
+
+
+
