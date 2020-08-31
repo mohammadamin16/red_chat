e3047f8a9c155a36e485c77d99591cd95a4ccbe9
Amin <toutounchi.ma@gmail.com>
2020-08-31 23:09:34 +0430

    reconnect after disconnection add nice design

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
 import React, {Component, useRef, useState} from 'react';
 import './App.css';
+
+import ReconnectingWebSocket from 'reconnecting-websocket';
+
 import Message from "./Message";
 
-// const url = 'ws://localhost:8000/ws/chat'
-const url = 'wss://super-nice-chat-app-backend.herokuapp.com/ws/chat'
+const url = 'ws://localhost:8000/ws/chat'
+
+// const url = 'wss://super-nice-chat-app-backend.herokuapp.com/ws/chat'
 
 
 class App extends Component {
 
     constructor(props) {
         super(props);
-        this.ws = new WebSocket(url)
+        this.ws = new ReconnectingWebSocket(url)
         this.state = {
+            name: '',
             message: '',
-            messages: []
+            messages: [],
+            status: 'Connecting...'
         }
     }
 
-
-    componentDidMount() {
+    connect = () => {
+        this.ws = new ReconnectingWebSocket(url)
         this.ws.onopen = () => {
             // on connecting, do nothing but log it to the console
+            clearInterval(this.retry)
             console.log('connected')
+            this.setState({status: 'Connected :)'})
         }
         this.ws.onmessage = (e) => {
             const data = JSON.parse(e.data)
             console.log(data)
-            if(data['type'] === 'old_messages'){
+            if (data['type'] === 'old_messages') {
                 let messages = data['messages']
                 this.setState({messages: messages})
-            }else if (data['type'] === 'new_message') {
+            } else if (data['type'] === 'new_message') {
                 let new_messages = this.state.messages
                 new_messages.push(data['message'])
                 this.setState({messages: new_messages})
@@ -38,31 +46,86 @@ class App extends Component {
 
         this.ws.onclose = () => {
             console.log('disconnected')
-            // automatically try to reconnect on connection loss
+            this.setState({status: 'Disconnected :('})
 
         }
     }
 
-    send = () => {
-        this.ws.send(JSON.stringify({'message': {body: this.state.message}}))
+    componentDidMount() {
+        this.connect()
+    }
+
+    send = (e) => {
+        if (this.state.name !== '' && this.state.message !== '') {
+
+            try {
+                e.preventDefault()
+            } catch (e) {
+            }
+            this.setState({message: ''})
+            this.ws.send(JSON.stringify({
+                'message': {
+                    body: this.state.message,
+                    name: this.state.name
+                }
+            }))
+        }else{
+            if (this.state.name === ''){
+                alert('Please pick a name')
+            }else{
+                alert("Message can't be empty can it? :)")
+            }
+
+        }
     }
 
     render() {
         return (
-
             <div className="App">
-                {this.state.messages.map((message) => (
-                    <Message
-                        message={message}
-                    />
-                ))}
 
-                <input
-                    onChange={(e) => {
-                        this.setState({message:e.target.value})
-                    }}
-                />
-                <button onClick={this.send}>Send</button>
+                <div className='message_container'>
+                    {this.state.messages.map((message) => (
+                        <Message
+                            username={this.state.name}
+                            message={message}
+                        />
+                    ))}
+                </div>
+                <form onSubmit={this.send} className='input_row'>
+                    <p>{this.state.status}</p>
+
+                    <input
+                        className='name_input'
+
+                        value={this.state.name}
+                        onChange={(e) => {
+                            this.setState({name: e.target.value})
+                        }}
+                        placeholder={'Name'}
+                    />
+
+
+                    <input
+                        className='message_input'
+                        value={this.state.message}
+                        onChange={(e) => {
+                            this.setState({message: e.target.value})
+                        }}
+                        placeholder={'Hit Enter to send'}
+                        onKeyPress={(e) => {
+                            if (e.key === 'Enter') {
+                                this.send()
+                            }
+                        }}
+                    />
+
+
+                    <div
+                        className='send_btn'
+                        onClick={this.send}>Send
+                    </div>
+
+                </form>
 
             </div>
         );
--- src/Message.js
+++ src/Message.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
 
-export default function Message(props){
-  return (
-    <p
-      className='message'
-    >{props.message.body}</p>
-  );
+export default function Message(props) {
+    if (props.username === props.message.name){
+        return (
+            <div className='message own_message' style={{alignSelf:'flex-end'}}>
+
+            <p className='message_body own_message_body'>{props.message.body}</p>
+        </div>
+    )}else {
+        return (
+            <div className='message' style={{alignSelf:'flex-start'}}>
+
+                <p className='message_name'>
+                    <span>{props.message.name.substring(0,2).toUpperCase()}</span></p>
+                <p className='full_name'>{props.message.name}</p>
+                <p className='message_body'>{props.message.body}</p>
+            </div>
+        );
+    }
 }
 
 
